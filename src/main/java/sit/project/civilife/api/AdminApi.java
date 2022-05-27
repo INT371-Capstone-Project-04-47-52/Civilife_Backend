67cc6fb2036b1f8705ce37019f60dc14644751bd
Noochajee Phonbooncharoenchai <noochajee.nn@mail.kmutt.ac.th>
2022-05-27 22:29:07 +0700

    [Add] admin api delete user

--- src/main/java/sit/project/civilife/api/AdminApi.java
+++ src/main/java/sit/project/civilife/api/AdminApi.java
@@ -1,8 +1,8 @@
 package sit.project.civilife.api;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import sit.project.civilife.exception.BaseException;
+import sit.project.civilife.exception.UserException;
 import sit.project.civilife.models.User;
 import sit.project.civilife.repositories.UserRepository;
 
@@ -24,6 +24,14 @@ public class AdminApi {
     @GetMapping("/allUsers")
     public Iterable<User> allUsers() {
         return userRepository.findAll();
+    }
+
+    @DeleteMapping("/delete/user/{userId}")
+    public void deleteUser(@PathVariable Long userId) throws BaseException {
+        if (!userRepository.existsById(userId)) {
+            throw UserException.userIdNotfound();
+        } else
+            userRepository.deleteById(userId);
     }
 
 }
