60c066dbfc8b9f01a30eea01f09419f4e8242fa5
Noochajee Phonbooncharoenchai <noochajee.nn@mail.kmutt.ac.th>
2022-05-28 04:42:14 +0700

    [Update] config

--- src/main/java/sit/project/civilife/config/WebSecurityConfig.java
+++ src/main/java/sit/project/civilife/config/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package sit.project.civilife.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,15 +9,23 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import sit.project.civilife.config.token.TokenFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import sit.project.civilife.config.token.TokenFilterConfigurer;
 import sit.project.civilife.service.TokenService;
+
+import java.util.Arrays;
 
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final TokenService tokenService;
-
+    @Value("#{'${civilife.origin.method}'.split(',')}")
+    private String[] methodList;
+    @Value("#{'${civilife.origin.host}'.split(',')}")
+    private String[] hostList;
+    @Value("#{'${civilife.origin.header}'.split(',')}")
+    private String[] headerList;
     public WebSecurityConfig(TokenService tokenService) {
         this.tokenService = tokenService;
     }
@@ -33,8 +42,20 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors()
-                .disable().csrf().disable()
+        http.cors(
+                        config -> {
+                            CorsConfiguration cors = new CorsConfiguration();
+                            cors.setAllowCredentials(true);
+                            cors.setAllowedOrigins(Arrays.asList(hostList));
+                            cors.setAllowedMethods(Arrays.asList(methodList));
+                            cors.setAllowedHeaders(Arrays.asList(headerList));
+
+                            UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+                            source.registerCorsConfiguration("/**", cors);
+
+                            config.configurationSource(source);
+                        }
+                ).csrf().disable().csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
